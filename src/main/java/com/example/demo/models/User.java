91fce674c51ab0846a8a1fb7323aff6e1d00211c
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-04-27 13:46:49 -0300

    Adjustment to the User model

--- src/main/java/com/example/demo/models/User.java
+++ src/main/java/com/example/demo/models/User.java
@@ -2,24 +2,26 @@ package com.example.demo.models;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import org.springframework.scheduling.config.Task;
+import com.example.demo.models.Task;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
-import java.util.Objects;
+
 
 @Entity
 @Table(name = User.TABLE_NAME)
@@ -51,7 +53,8 @@ public class User {
     @Size(groups = { CreateUser.class, UpdateUser.class }, min = 8, max = 60)
     private String password;
 
-    // private List<Task> tasks = new ArrayList<Task>();
+    @OneToMany(mappedBy = "user")
+    private List<Task> tasks = new ArrayList<Task>();
 
     public User() {
     }
@@ -85,6 +88,16 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+
+    public List<Task> getTasks() {
+        return this.tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+    
 
     @Override
     public boolean equals(Object obj) {
